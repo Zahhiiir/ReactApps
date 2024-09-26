33f9e1a974abe5df1120043153ec120be77cb9e1
Zahhiiir <zahirshaikhedu@gmail.com>
2024-09-26 21:49:50 +0530

    new component created

---
+++ hookTest/src/components/Fruits.jsx
@@ -0,0 +1,4 @@
+export default function Fruits() {
+  const fruits = ["Apple", "Banana", "Mango", "Avacardo"];
+  return <div>{fruits}</div>;
+}
--- hookTest/src/main.jsx
+++ hookTest/src/main.jsx
@@ -2,9 +2,15 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
+import { Fruits } from "./components/Fruits";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <>
+      <App name="zahir" />
+      <App name="theleel" />
+      <App name="zaid" />
+      <Fruits />
+    </>
   </StrictMode>
 );
